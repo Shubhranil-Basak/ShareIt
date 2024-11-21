ef6a1b6085ad112fee2e3c74e4de911dd17f8ce8
Aishwarya Sharma <aishsharma1102@gmail.com>
2024-11-22 00:06:49 +0530

    Updated user methods

--- user.cpp
+++ user.cpp
@@ -1,25 +1,105 @@
+// #include "classes.h"
+
+// using namespace std;
+
+// User::User(string &username, string &password):
+//     username(username), password(password), coins(100) {}
+
+// bool User::authenticate(string &entered_username, string &entered_password) const {
+//     return this->username == entered_username && this->password == entered_password;
+// }
+
+// string User::getUsername() const {
+//     return this->username;
+// }
+
+// void User::listItem(Listing* listing) {
+//     this->listings.push_back(listing);
+// }
+
+// void User::removeListing(Listing* listing) {
+//     Listing* listing_to_del = nullptr;
+//     for (int i = 0; i < this->listings.size(); i++) {
+//         if (this->listings[i] == listing) {
+//             listing_to_del = this->listings[i];
+//             this->listings.erase(this->listings.begin() + i);
+//             break;
+//         }
+//     }
+//     delete listing_to_del;
+//     listing_to_del = nullptr;
+// }
+
+// void User::requestItem(Item* requested_item) {
+//     this->requested_items.push_back(requested_item);
+// }
+
+// void User::removeRequest(string &name) {
+//     Item* item_to_del = nullptr;
+//     for (int i = 0; i < this->requested_items.size(); i++) {
+//         if (this->requested_items[i]->getName() == name) {
+//             item_to_del = this->requested_items[i];
+//             this->requested_items.erase(this->requested_items.begin() + i);
+//             break;
+//         }
+//     }
+//     delete item_to_del;
+//     item_to_del = nullptr;
+// }
+
+// void User::borrowItem(Item *item) {
+//     borrowed_items.push_back(item);
+// }
+
+// void User::printNotifications() const {
+//     cout << "Notifications (Total: " << this->notifications.size() << ")" << endl;
+//     cout << separator << endl;
+//     for (Notification* notification: this->notifications) {
+//         notification->printNotification();
+//         cout << separator << endl;
+//     }
+// }
+
+// void User::acceptCoins(int coins_given) {
+//     this->coins += coins_given;
+// }
+
+// bool User::spendCoins(int coins_to_spend) {
+//     if (coins_to_spend > coins)
+//         return false;
+//     coins -= coins_to_spend;
+//     return true;
+// }
+
 #include "classes.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-User::User(string &username, string &password):
-    username(username), password(password), coins(100) {}
+// Constructor
+User::User(string &username, string &password)
+    : username(username), password(password), coins(100) {}
 
+// Authentication
 bool User::authenticate(string &entered_username, string &entered_password) const {
     return this->username == entered_username && this->password == entered_password;
 }
 
+// Getters
 string User::getUsername() const {
     return this->username;
 }
 
-void User::listItem(Listing* listing) {
+// Listings Management
+void User::listItem(Listing *listing) {
     this->listings.push_back(listing);
 }
 
-void User::removeListing(Listing* listing) {
-    Listing* listing_to_del = nullptr;
-    for (int i = 0; i < this->listings.size(); i++) {
+void User::removeListing(Listing *listing) {
+    Listing *listing_to_del = nullptr;
+    for (size_t i = 0; i < this->listings.size(); i++) {
         if (this->listings[i] == listing) {
             listing_to_del = this->listings[i];
             this->listings.erase(this->listings.begin() + i);
@@ -30,13 +110,14 @@ void User::removeListing(Listing* listing) {
     listing_to_del = nullptr;
 }
 
-void User::requestItem(Item* requested_item) {
+// Request Management
+void User::requestItem(Item *requested_item) {
     this->requested_items.push_back(requested_item);
 }
 
 void User::removeRequest(string &name) {
-    Item* item_to_del = nullptr;
-    for (int i = 0; i < this->requested_items.size(); i++) {
+    Item *item_to_del = nullptr;
+    for (size_t i = 0; i < this->requested_items.size(); i++) {
         if (this->requested_items[i]->getName() == name) {
             item_to_del = this->requested_items[i];
             this->requested_items.erase(this->requested_items.begin() + i);
@@ -47,26 +128,58 @@ void User::removeRequest(string &name) {
     item_to_del = nullptr;
 }
 
+// Borrowing Management
 void User::borrowItem(Item *item) {
-    borrowed_items.push_back(item);
+    this->borrowed_items.push_back(item);
 }
 
+// Notifications
 void User::printNotifications() const {
     cout << "Notifications (Total: " << this->notifications.size() << ")" << endl;
     cout << separator << endl;
-    for (Notification* notification: this->notifications) {
+    for (Notification *notification : this->notifications) {
         notification->printNotification();
         cout << separator << endl;
     }
 }
 
+void User::addNotification(Notification *notification) {
+    this->notifications.push_back(notification);
+}
+
+void User::clearNotifications() {
+    for (Notification *notification : this->notifications) {
+        delete notification;
+    }
+    this->notifications.clear();
+}
+
+// Coins Management
 void User::acceptCoins(int coins_given) {
     this->coins += coins_given;
 }
 
 bool User::spendCoins(int coins_to_spend) {
-    if (coins_to_spend > coins)
+    if (coins_to_spend > this->coins)
         return false;
-    coins -= coins_to_spend;
+    this->coins -= coins_to_spend;
     return true;
+}
+
+int User::getCoinBalance() const {
+    return this->coins;
+}
+
+// Destructor
+User::~User() {
+    for (Listing *listing : this->listings) {
+        delete listing;
+    }
+    for (Item *item : this->requested_items) {
+        delete item;
+    }
+    for (Item *item : this->borrowed_items) {
+        delete item;
+    }
+    clearNotifications();
 }
